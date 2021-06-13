9a7e39b421925e3bc27f26fb543f6d6d59da6700
dozigden <luke@savageconclave.com>
2021-06-13 16:16:15 +0100

    Port previous changes from local build

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/dozigden/config.h
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/dozigden/config.h
@@ -18,10 +18,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-
 #define USE_SERIAL
 
-#define MASTER_LEFT
+#define SPLIT_HAND_PIN B7
+// #define MASTER_LEFT
 // #define MASTER_RIGHT
-//#define EE_HANDS
+// #define EE_HANDS
 // Rows are doubled-up
